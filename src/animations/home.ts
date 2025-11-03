7a0322ca932a64b28cbb1d30f40994753b47308e
Welton Nogueira <welton@internit.net.br>
2025-11-03 08:18:34 -0300

    fix

--- src/animations/home.ts
+++ src/animations/home.ts
@@ -26,5 +26,51 @@ export const home: Wrapper = {
       });
   },
 
-  whoWeAre: (container) => {},
+  whoWeAre: (container) => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: container,
+          start: "top 80%",
+        },
+      })
+      .from("[data-el='strong']", {
+        autoAlpha: 0,
+        ease: "power1.inOut",
+      })
+      .from("h2", {
+        autoAlpha: 0,
+        y: 16,
+        ease: "power1.inOut",
+      })
+      .from("[data-el='data'] p", {
+        autoAlpha: 0,
+        stagger: 0.2,
+        ease: "power1.inOut",
+      })
+      .from("[data-el='cta']", {
+        autoAlpha: 0,
+        x: -16,
+        ease: "power1.inOut",
+      })
+      .from("[data-el='count']", {
+        textContent: "0",
+        ease: "power1.inOut",
+        duration: 3,
+        snap: {
+          textContent: 1,
+        },
+      });
+
+    gsap.from("[data-el='card']", {
+      scrollTrigger: {
+        trigger: "[data-el='card']",
+        start: "top 80%",
+      },
+      autoAlpha: 0,
+      y: 16,
+      stagger: 0.2,
+      ease: "power1.inOut",
+    });
+  },
 };
--- src/components/pages/home/who-we-are.tsx
+++ src/components/pages/home/who-we-are.tsx
@@ -11,7 +11,10 @@ export function WhoWeAre() {
         <div className="mx-auto max-w-screen-2xl px-4 py-12 lg:px-8 lg:py-24">
           <div className="grid items-center gap-12 lg:grid-cols-2 lg:gap-24">
             <div>
-              <strong className="text-prime-red flex items-center gap-2 font-normal uppercase">
+              <strong
+                data-el="strong"
+                className="text-prime-red flex items-center gap-2 font-normal uppercase"
+              >
                 <Cards className="stroke-prime-red size-6" />
                 Quem Somos
               </strong>
@@ -20,7 +23,10 @@ export function WhoWeAre() {
                 Onde a paixão pelo poker encontra a excelência
               </h2>
 
-              <div className="text-prime-light mt-6 flex flex-col gap-4">
+              <div
+                data-el="data"
+                className="text-prime-light mt-6 flex flex-col gap-4"
+              >
                 <p>
                   O Prime Poker Team é a equipe de poker mais vencedora do
                   Brasil, formada por jogadores profissionais que já acumularam
@@ -35,21 +41,23 @@ export function WhoWeAre() {
                 </p>
               </div>
 
-              <Link
-                href="#"
-                className="bg-prime-red text-prime-light hover:bg-prime-light hover:text-prime-red mt-8 flex h-14 w-fit items-center gap-2 rounded-sm px-4 font-medium transition-all duration-500"
-              >
-                Evolua com a Prime Poker
-                <CaretRightIcon className="size-6" />
-              </Link>
+              <div data-el="cta">
+                <Link
+                  href="#"
+                  className="bg-prime-red text-prime-light hover:bg-prime-light hover:text-prime-red mt-8 flex h-14 w-fit items-center gap-2 rounded-sm px-4 font-medium transition-all duration-500"
+                >
+                  Evolua com a Prime Poker
+                  <CaretRightIcon className="size-6" />
+                </Link>
+              </div>
             </div>
 
             <div className="grid place-items-center">
               <div className="flex w-fit flex-col gap-4">
                 <div className="flex items-center gap-4">
                   <div className="flex flex-col rounded-md border-white/3 bg-white/3 px-8 py-4">
-                    <h3 className="text-4xl font-bold text-prime-red/75 lg:text-5xl">
-                      05+
+                    <h3 className="text-prime-red/75 text-4xl font-bold lg:text-5xl">
+                      <span data-el="count">5</span>+
                     </h3>
                     <p className="text-sm tracking-wider text-gray-400 uppercase">
                       Anos no mercado
@@ -57,8 +65,8 @@ export function WhoWeAre() {
                   </div>
 
                   <div className="flex flex-col rounded-md border-white/3 bg-white/3 px-8 py-4">
-                    <h3 className="text-4xl font-bold text-prime-red/75 lg:text-5xl">
-                      150+
+                    <h3 className="text-prime-red/75 text-4xl font-bold lg:text-5xl">
+                      <span data-el="count">150</span>+
                     </h3>
                     <p className="text-sm tracking-wider text-gray-400 uppercase">
                       Jogadores formados
@@ -67,8 +75,10 @@ export function WhoWeAre() {
                 </div>
 
                 <div className="flex flex-col items-center rounded-md border-white/3 bg-white/3 px-8 py-4">
-                  <h3 className="text-4xl font-bold text-prime-red/75 lg:text-5xl">
-                    $ 2.25M+
+                  <h3 className="text-prime-red/75 text-4xl font-bold lg:text-5xl">
+                    $ <span data-el="count">2</span>.
+                    <span data-el="count">25</span>
+                    M+
                   </h3>
                   <p className="text-sm tracking-wider text-gray-400 uppercase">
                     Em ganhos acumulados
@@ -87,7 +97,7 @@ export function WhoWeAre() {
           />
 
           <div className="text-prime-light mt-12 grid gap-8 lg:mt-24 lg:grid-cols-3 lg:gap-16">
-            <div className="flex flex-col gap-4">
+            <div data-el="card" className="flex flex-col gap-4">
               <h3 className="text-prime-light mt-2 gap-2 text-3xl font-bold uppercase">
                 Visão
               </h3>
@@ -100,7 +110,7 @@ export function WhoWeAre() {
               </p>
             </div>
 
-            <div className="flex flex-col gap-4">
+            <div data-el="card" className="flex flex-col gap-4">
               <h3 className="text-prime-light mt-2 gap-2 text-3xl font-bold uppercase">
                 Missão
               </h3>
@@ -113,7 +123,7 @@ export function WhoWeAre() {
               </p>
             </div>
 
-            <div className="flex flex-col gap-4">
+            <div data-el="card" className="flex flex-col gap-4">
               <h3 className="text-prime-light mt-2 gap-2 text-3xl font-bold uppercase">
                 Valores
               </h3>
